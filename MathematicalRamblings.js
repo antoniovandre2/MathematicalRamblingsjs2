0502b3678fec0767ba722a03c7d70f19170811c7
Antonio Vandré Pedrosa Furtunato Gomes <90938909+antoniovandre2@users.noreply.github.com>
2022-02-14 18:40:07 -0300

    Updates.

--- MathematicalRamblings.js
+++ MathematicalRamblings.js
@@ -6,7 +6,7 @@
 
 // Sugestão ou comunicar erro: "a.vandre.g@gmail.com".
 
-// Última atualização: 09-02-2022. Não considerando alterações em macros.
+// Última atualização: 14-02-2022. Não considerando alterações em macros.
 
 // Início escopo desenvolvido por Antonio Vandré Pedrosa Furtunato Gomes (bit.ly/antoniovandre_legadoontologico).
 
@@ -16,7 +16,7 @@ console.log("                                                  \n          .\',;
 
 // Versão do MathematicalRamblings.js. Não considerando alterações em macros.
 
-function antoniovandremathematicalramblingsjsversao(){return "09-02-2022";}
+function antoniovandremathematicalramblingsjsversao(){return "14-02-2022";}
 
 // Fim mensagem de inicialização no console.log.
 
@@ -10654,6 +10654,72 @@ function antoniovandrepontosimetricoreta(str, retorno)
 		default:
 			return "e";
 		}
+	}
+
+// Retornando string de caracteres ASDII para string de códigos binários como argumento. Retorna NULL se um erro genérico ocorre.
+
+function antoniovandrebinascii(str, avisoanexo)
+	{
+	if (avisoanexo == -1) return "Traduz apenas os caracteres codes 32-127. Dentre os caracteres não echoáveis, há suporte apenas para o CR (Carriage return) e HT (tabulação).";
+
+	var tabelaascii = [["00001001","\t"],["00001101","\n"],["00100001","!"],["00100010","\""],["00100011","#"],["00100100","$"],["00100101","%"],["00100110","&"],["00100111","\'"],["00101000","("],["00101001",")"],["00101010","*"],["00101011","+"],["00101100",","],["00101101","-"],["00101110","."],["00101111","/"],["00110000","0"],["00110001","1"],["00110010","2"],["00110011","3"],["00110100","4"],["00110101","5"],["00110110","6"],["00110111","7"],["00111000","8"],["00111001","9"],["00111010",":"],["00111011",";"],["00111100","<"],["00111101","="],["00111110",">"],["00111111","?"],["01000000","@"],["01000001","A"],["01000010","B"],["01000011","C"],["01000100","D"],["01000101","E"],["01000110","F"],["01000111","G"],["01001000","H"],["01001001","I"],["01001010","J"],["01001011","K"],["01001100","L"],["01001101","M"],["01001110","N"],["01001111","O"],["01010000","P"],["01010001","Q"],["01010010","R"],["01010011","S"],["01010100","T"],["01010101","U"],["01010110","V"],["01010111","W"],["01011000","X"],["01011001","Y"],["01011010","Z"],["01011011","["],["01011100","\\"],["01011101","]"],["01011110","^"],["01011111","_"],["01100000","`"],["01100001","a"],["01100010","b"],["01100011","c"],["01100100","d"],["01100101","e"],["01100110","f"],["01100111","g"],["01101000","h"],["01101001","i"],["01101010","j"],["01101011","k"],["01101100","l"],["01101101","m"],["01101110","n"],["01101111","o"],["01110000","p"],["01110001","q"],["01110010","r"],["01110011","s"],["01110100","t"],["01110101","u"],["01110110","v"],["01110111","w"],["01111000","x"],["01111001","y"],["01111010","z"],["01111011","{"],["01111100","|"],["01111101","}"],["01111110","~"],["00100000", " "]];
+
+	var argumentos = str.split(";");
+	var buffer = "";
+	var strout = "";
+
+	if (argumentos.length != 2) return null;
+
+	if ((antoniovandrecompararstrings(typeof argumentos[0], "string") == "e") || (antoniovandrecompararstrings(typeof argumentos[1], "string") == "e")) return null;
+
+	if ((argumentos[1] == "a") && (argumentos[0].length % 8 != 0)) return "Não há conjuntos completos de 8 bits no código binário.";
+
+	if (argumentos[1].trim() == "a") for (var i = 0; i < argumentos[0].length; i++)
+		{
+		if (((i + 1) % 8 == 0) && (i != 0))
+			{
+			var flag = 0;
+
+			buffer = buffer + argumentos[0][i];
+
+			for (var j = 0; j < tabelaascii.length; j++)
+				if (antoniovandrecompararstrings(buffer, tabelaascii[j][0]) == 1)
+					{
+					strout = strout + tabelaascii[j][1];
+					flag = 1;
+					break;
+					}
+
+			if (flag == 0) return "Um ou mais códigos binários não reconhecidos.";
+
+			buffer = "";
+			}
+		else
+			{
+			buffer = buffer + argumentos[0][i];
+			}
+		}
+	else if (argumentos[1].trim() == "b")
+		{
+		for (var i = 0; i < argumentos[0].length; i++)
+			{
+			var flag = 0;
+
+			for (var j = 0; j < tabelaascii.length; j++)
+			if (argumentos[0][i] == tabelaascii[j][1])
+				{
+				strout = strout + tabelaascii[j][0];
+				flag = 1;
+				break;
+				}
+
+			if (flag == 0) return "Um ou mais caracteres não reconhecidos.";
+			}
+		}
+	else
+		return null;
+
+	return strout;
 	}
 
 // Fim escopo desenvolvido por Antonio Vandré Pedrosa Furtunato Gomes (bit.ly/antoniovandre_legadoontologico).
