b4137774fbf52b0aab1c4b27687843f58a2a3089
Antonio Vandré Pedrosa Furtunato Gomes <90938909+antoniovandre2@users.noreply.github.com>
2025-02-25 18:25:22 -0300

    Mais atualizações.

--- HackTimer.js
+++ HackTimer.js
@@ -1,147 +1,103 @@
-(function (workerScript) {
-	if (!/MSIE 10/i.test (navigator.userAgent)) {
-		try {
-			var blob = new Blob (["\
-var fakeIdToId = {};\
-onmessage = function (event) {\
-	var data = event.data,\
-		name = data.name,\
-		fakeId = data.fakeId,\
-		time;\
-	if(data.hasOwnProperty('time')) {\
-		time = data.time;\
-	}\
-	switch (name) {\
-		case 'setInterval':\
-			fakeIdToId[fakeId] = setInterval(function () {\
-				postMessage({fakeId: fakeId});\
-			}, time);\
-			break;\
-		case 'clearInterval':\
-			if (fakeIdToId.hasOwnProperty (fakeId)) {\
-				clearInterval(fakeIdToId[fakeId]);\
-				delete fakeIdToId[fakeId];\
-			}\
-			break;\
-		case 'setTimeout':\
-			fakeIdToId[fakeId] = setTimeout(function () {\
-				postMessage({fakeId: fakeId});\
-				if (fakeIdToId.hasOwnProperty (fakeId)) {\
-					delete fakeIdToId[fakeId];\
-				}\
-			}, time);\
-			break;\
-		case 'clearTimeout':\
-			if (fakeIdToId.hasOwnProperty (fakeId)) {\
-				clearTimeout(fakeIdToId[fakeId]);\
-				delete fakeIdToId[fakeId];\
-			}\
-			break;\
-	}\
-}\
-"]);
-			// Obtain a blob URL reference to our worker 'file'.
-			workerScript = window.URL.createObjectURL(blob);
-		} catch (error) {
-			/* Blob is not supported, use external script instead */
-		}
+// ==UserScript==
+// @name         hacktimerjs
+// @namespace    http://tampermonkey.net/
+// @version      1.1.0
+// @description  浏览器后台
+// @author       turuslan
+// @grant        none
+// ==/UserScript==
+(function(s) {
+	var w, f = {},
+	o = window,
+	l = console,
+	m = Math,
+	z = 'postMessage',
+	x = 'HackTimer.js by turuslan: ',
+	v = 'Initialisation failed',
+	p = 0,
+	r = 'hasOwnProperty',
+	y = [].slice,
+	b = o.Worker;
+	function d() {
+		do {
+			p = 0x7FFFFFFF > p ? p + 1 : 0
+		} while ( f [ r ](p));
+		return p
 	}
-	var worker,
-		fakeIdToCallback = {},
-		lastFakeId = 0,
-		maxFakeId = 0x7FFFFFFF, // 2 ^ 31 - 1, 31 bit, positive values of signed 32 bit integer
-		logPrefix = 'HackTimer.js by turuslan: ';
-	if (typeof (Worker) !== 'undefined') {
-		function getFakeId () {
-			do {
-				if (lastFakeId == maxFakeId) {
-					lastFakeId = 0;
-				} else {
-					lastFakeId ++;
-				}
-			} while (fakeIdToCallback.hasOwnProperty (lastFakeId));
-			return lastFakeId;
-		}
+	if (!/MSIE 10/i.test(navigator.userAgent)) {
 		try {
-			worker = new Worker (workerScript);
-			window.setInterval = function (callback, time /* , parameters */) {
-				var fakeId = getFakeId ();
-				fakeIdToCallback[fakeId] = {
-					callback: callback,
-					parameters: Array.prototype.slice.call(arguments, 2)
-				};
-				worker.postMessage ({
-					name: 'setInterval',
-					fakeId: fakeId,
-					time: time
-				});
-				return fakeId;
-			};
-			window.clearInterval = function (fakeId) {
-				if (fakeIdToCallback.hasOwnProperty(fakeId)) {
-					delete fakeIdToCallback[fakeId];
-					worker.postMessage ({
-						name: 'clearInterval',
-						fakeId: fakeId
-					});
-				}
-			};
-			window.setTimeout = function (callback, time /* , parameters */) {
-				var fakeId = getFakeId ();
-				fakeIdToCallback[fakeId] = {
-					callback: callback,
-					parameters: Array.prototype.slice.call(arguments, 2),
-					isTimeout: true
-				};
-				worker.postMessage ({
-					name: 'setTimeout',
-					fakeId: fakeId,
-					time: time
-				});
-				return fakeId;
-			};
-			window.clearTimeout = function (fakeId) {
-				if (fakeIdToCallback.hasOwnProperty(fakeId)) {
-					delete fakeIdToCallback[fakeId];
-					worker.postMessage ({
-						name: 'clearTimeout',
-						fakeId: fakeId
-					});
-				}
-			};
-			worker.onmessage = function (event) {
-				var data = event.data,
-					fakeId = data.fakeId,
-					request,
-					parameters,
-					callback;
-				if (fakeIdToCallback.hasOwnProperty(fakeId)) {
-					request = fakeIdToCallback[fakeId];
-					callback = request.callback;
-					parameters = request.parameters;
-					if (request.hasOwnProperty ('isTimeout') && request.isTimeout) {
-						delete fakeIdToCallback[fakeId];
-					}
-				}
-				if (typeof (callback) === 'string') {
-					try {
-						callback = new Function (callback);
-					} catch (error) {
-						console.log (logPrefix + 'Error parsing callback code string: ', error);
-					}
-				}
-				if (typeof (callback) === 'function') {
-					callback.apply (window, parameters);
-				}
-			};
-			worker.onerror = function (event) {
-				console.log (event);
-			};
-		} catch (error) {
-			console.log (logPrefix + 'Initialisation failed');
-			console.error (error);
-		}
-	} else {
-		console.log (logPrefix + 'Initialisation failed - HTML5 Web Worker is not supported');
+			s = o.URL.createObjectURL(new Blob(["var f={},p=postMessage,r='hasOwnProperty';onmessage=function(e){var d=e.data,i=d.i,t=d[r]('t')?d.t:0;switch(d.n){case'a':f[i]=setInterval(function(){p(i)},t);break;case'b':if(f[r](i)){clearInterval(f[i]);delete f[i]}break;case'c':f[i]=setTimeout(function(){p(i);if(f[r](i))delete f[i]},t);break;case'd':if(f[r](i)){clearTimeout(f[i]);delete f[i]}break}}"]))
+		} catch(e) {}
 	}
-}) ('HackTimerWorker.js');
+	if (typeof(b) !== 'undefined') {
+		try {
+			w = new b(s);
+			console.log('test');
+			o.setInterval = function(c, t) {
+							console.log('tes1t');
+
+				var i = d();
+				f[i] = {
+					c: c,
+					p: y.call(arguments, 2)
+				};
+				w[z]({
+					n: 'a',
+					i: i,
+					t: t
+				});
+				return i
+			};
+			o.clearInterval = function(i) {
+				if (f[r](i)) delete f[i],
+				w[z]({
+					n: 'b',
+					i: i
+				})
+			};
+			o.setTimeout = function(c, t) {
+				var i = d();
+				f[i] = {
+					c: c,
+					p: y.call(arguments, 2),
+					t: !0
+				};
+				w[z]({
+					n: 'c',
+					i: i,
+					t: t
+				});
+				return i
+			};
+			o.clearTimeout = function(i) {
+				if (f[r](i)) delete f[i],
+				w[z]({
+					n: 'd',
+					i: i
+				})
+			};
+			w.onmessage = function(e) {
+				var i = e.data,
+				c, n;
+				if (f[r](i)) {
+					n = f[i];
+					c = n.c;
+					if (n[r]('t')) delete f[i]
+				}
+				if (typeof(c) == 'string') try {
+					c = new Function(c)
+				} catch(k) {
+					l.log(x + 'Error parsing callback code string: ', k)
+				}
+				if (typeof(c) == 'function') c.apply(o, n.p)
+			};
+			w.onerror = function(e) {
+				l.log(e)
+			};
+			l.log(x + 'Initialisation succeeded')
+		} catch(e) {
+			l.log(x + v);
+			l.error(e)
+		}
+	} else l.log(x + v + ' - HTML5 Web Worker is not supported')
+})('HackTimerWorker.min.js');
